7a8a1724023e30bec8aa61163b6d4aa9a25a0ebc
HoangAnh <you@example.com>
2020-04-11 22:01:40 +0700

    Complete Authentication

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -29,6 +29,8 @@ module.exports.postLogin = (req, res) => {
 		})
 		return
 	}
-
+	res.cookie('userId', user.id,{
+		signed : true
+	})
 	res.redirect('/')
 }
--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const port = 3000
 
 const signinRoute = require('./routes/auth.route')
 const articleRoute = require('./routes/article.route')
+const authMiddleware = require('./middleWare/auth.middleware')
 
+var cookieParser = require('cookie-parser')
+
+app.use(cookieParser('saasefawef3r23fecas'))
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
@@ -17,7 +21,8 @@ app.get('/', (req, res) =>{
 })
 
 app.use('/auth',signinRoute)
-app.use('/article', articleRoute)
+
+app.use('/article',authMiddleware.requireAuth, articleRoute)
 
 
 
